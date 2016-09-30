60bc4cd6d7022aab801ff07f15f7d12331b126a4
HummyPkg <af123@hummypkg.org.uk>
2016-09-30 12:31:12 +0100

    bump_colour now returns new colour list

--- ext/bump_colour.tcl
+++ ext/bump_colour.tcl
@@ -3,6 +3,6 @@ proc bump_colour {{bump 30}} {{colour 0}} {
 	incr colour $bump
 	if {$colour > 189} { incr colour -190 }
 	if {$colour < 0} { incr colour 189 }
-	cube.colour $colour
+	return [cube.colour $colour]
 }
 
