5c53830b3bccf38e7c40baf45e5f88b13f18ac39
HummyPkg <af123@hummypkg.org.uk>
2016-10-04 14:36:15 +0100

    allow specific starting colour to be set

--- ext/bump_colour.tcl
+++ ext/bump_colour.tcl
@@ -1,6 +1,10 @@
 
-proc bump_colour {{bump 30}} {{colour 0}} {
-	incr colour $bump
+proc bump_colour {{bump 30} {fc 0}} {{colour 0}} {
+	if {!$bump} {
+		set colour $fc
+	} else {
+		incr colour $bump
+	}
 	if {$colour > 189} { incr colour -190 }
 	if {$colour < 0} { incr colour 189 }
 	return [cube.colour $colour]
